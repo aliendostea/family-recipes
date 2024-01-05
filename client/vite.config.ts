94fb2793e7146c5ec2d300d6fdf63fb5848f7a2c
aliendostea <aliendostea@gmail.com>
2024-01-05 12:35:59 +0100

    client: setup test, vitest, testing library

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,15 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTests",
+    cache: {
+      dir: "./node_modules/.vitest",
+    },
+    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/components"),
